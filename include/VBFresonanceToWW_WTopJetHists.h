41a1d46749188e5a1fb96a218f9c346515dae565
Irene <irene.zoi@cern.ch>
2017-05-26 15:59:27 +0200

    Starting Analysis selection

---
+++ include/VBFresonanceToWW_WTopJetHists.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "UHH2/core/include/Hists.h"
+#include "UHH2/core/include/Event.h"
+//#include "ZPrimeTotTPrimeGenSelections.h"
+#include "UHH2/core/include/LorentzVector.h"
+/**
+ *
+ *Histograms for GenTopJet
+ *
+ **/
+
+class VBFresonanceToWW_WTopJetHists : public uhh2::Hists{
+  public:
+  VBFresonanceToWW_WTopJetHists(uhh2::Context & ctx, const std::string & dirname);//, const std::string & collection = " ");
+    virtual void fill(const uhh2::Event & event) override;
+
+ protected:
+    TH1F *pt, *eta, *phi, *mass, *chf, *tau1, *tau2;
+    //uhh2::Event::Handle<GenTopJet> h_gentopjets;
+    //    uhh2::Event::Handle<std::vector <TopJet>  > h_topjets;
+
+
+};
