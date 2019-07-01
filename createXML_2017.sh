adc5c110e43956281fd7d586e0ba5ca50a278f7f
Irene <irene.zoi@cern.ch>
2019-07-01 09:56:55 +0200

    xml files for 2017 F data high PU runs and 1 signal samplenfor nominal PUPPI, v4 and v6

---
+++ createXML_2017.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+#python /nfs/dust/cms/user/zoiirene/RunII_102X/CMSSW_10_2_10/src/UHH2/scripts/create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/workdir_data2017F_special_JetHT_PUPPI_presel/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_highPU*'
+#python /nfs/dust/cms/user/zoiirene/RunII_102X/CMSSW_10_2_10/src/UHH2/scripts/create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/workdir_data2017F_JetHT_PUPPI_presel/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F*'
+#python /nfs/dust/cms/user/zoiirene/RunII_102X/CMSSW_10_2_10/src/UHH2/scripts/create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/workdir_data2017F_JetHT_PUPPI_presel/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_PUPPIv4*'
+#python /nfs/dust/cms/user/zoiirene/RunII_102X/CMSSW_10_2_10/src/UHH2/scripts/create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/workdir_data2017F_special_JetHT_PUPPI_presel/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_highPU_PUPPIv4*'
+python /nfs/dust/cms/user/zoiirene/RunII_102X/CMSSW_10_2_10/src/UHH2/scripts/create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/workdir_data2017F_special_JetHT_PUPPIv6_presel/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_highPU_PUPPIv6*'
+#python /nfs/dust/cms/user/zoiirene/RunII_102X/CMSSW_10_2_10/src/UHH2/scripts/create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/workdir_data2017F_JetHT_PUPPI_presel/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_PUPPIv4v2*'
+#python /nfs/dust/cms/user/zoiirene/RunII_102X/CMSSW_10_2_10/src/UHH2/scripts/create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/workdir_data2018_JetHT_PUPPIv4WithNom_presel/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2018B_PUPPIv4WithNom*'
+
+#mv uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F.xml /nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/xmlfiles/
+#mv uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_hihgPU.xml /nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/xmlfiles/
+#mv uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_PUPPIv4v2.xml /nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/xmlfiles/
+#mv uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_PUPPIv4.xml /nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/xmlfiles/
+mv uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_highPU_PUPPIv6.xml /nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/xmlfiles/
+#mv uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2017v2F_highPU_PUPPIv4.xml /nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/xmlfiles/
+#mv uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2018B_PUPPIv4WithNom.xml /nfs/dust/cms/user/zoiirene/DiBoson/102X/OutputBatch/PreSelection/xmlfiles/
+
+rm TEMPLATE*xml
